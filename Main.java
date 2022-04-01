49ee72c7375db6c21f0916dd1f93ce868df055f9
patricktelnoni <patricktelnoni@users.noreply.github.com>
2022-04-01 06:58:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    Pegawai mark = new Pegawai();
+    mark.nama = "Mark";
+    mark.nip  = 10932;
+    
+    System.out.println(mark.nama);
+    mark.bekerja();
+
+  }
+}
---
+++ Pegawai.java
@@ -0,0 +1,8 @@
+public class Pegawai{
+  private String nama;
+  private int nip;
+
+  public void bekerja(){
+    System.out.println("Sedang bekerja");
+  }
+}
